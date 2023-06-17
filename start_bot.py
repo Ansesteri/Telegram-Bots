01bf0c8a5ca65d813b89d9f43fd6b91d11355865
Ansesteri <angrylemon5678@gmail.com>
2023-06-17 14:13:59 +0300

    Add mode select

--- start_bot.py
+++ start_bot.py
@@ -9,6 +9,15 @@ from user import User, DEFAULT_USER_LEVEL, get_or_create_user, save_user, del_us
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
+@bot.message_handler(commands=['mode'])
+def select_mode(message):
+   user = get_or_create_user(message.from_user.id)
+   user.mode = ''
+   user.reset()
+   response = 'Game "Bulls and Cows"\n' + \
+               'Chose moe (who guesses the number)'
+   bot.send_message(message.from_user.id, response, reply_markup=get_buttons('Bot', 'User'))
+
 @bot.message_handler(commands=['level'])
 def select_level(message):
    user = get_or_create_user(message.from_user.id)
@@ -16,19 +25,14 @@ def select_level(message):
    save_user(message.from_user.id, user)
    response = 'Game "Bulls and Cows"\n' + \
                'Chose level (digit count)'
-   bot.send_message(message.from_user.id, response, reply_markup=get_level_buttons())
-
-def get_level_buttons():
-   buttons = telebot.types.ReplyKeyboardMarkup(
-      one_time_keyboard=True,
-      resize_keyboard=True,
-   )
-   buttons.add('3', '4', '5')
-   return buttons
+   bot.send_message(message.from_user.id, response, reply_markup=get_buttons('3', '4', '5'))
 
 @bot.message_handler(commands=['start', 'game'])
 def start_game(message, level=None):
    user = get_or_create_user(message.from_user.id)
+   if not user.mode:
+      select_mode(message)
+      return
    if level:
       user.level = level
    digits = [s for s in string.digits]
@@ -57,53 +61,69 @@ After every try bot will say count of right numbers, numbers that are not in cor
 
 @bot.message_handler(content_types=['text'])
 def bot_answer(message):
-   text = message.text
    user = get_or_create_user(message.from_user.id)
    if user.number:
-      if len(text) == user.level and text.isnumeric() and len(text) == len(set(text)):
-         bulls, cows = get_bulls_cows(text, user.number)
-         user.tries += 1
-         if bulls != user.level:
-            response = f'Bulls: {bulls} | Cows: {cows} | Tries: {user.tries}'
-            save_user(message.from_user.id, user)
-         else:
-            if user.tries <= 3:
-               response = f'You guessed right really fast, only in {user.tries} tries, do you want to play again?'
-               user.reset()
-               save_user(message.from_user.id, user)
-               bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
-               return
-            elif user.tries >= 4 and user.tries < 8:
-               response = f'You guessed right in {user.tries} tries, do you want to play again?'
-               user.reset()
-               save_user(message.from_user.id, user)
-               bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
-               return
-            elif user.tries >= 8:
-               response = f'You guessed right really slow, it took you {user.tries} tries, do you want to play again?'
-               user.reset()
-               save_user(message.from_user.id, user)
-               bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
-               return
-      else:
-         response = f'Send {user.level}-digit number that has unique numbers!'
+      bot_answer_to_user_guess(message, user)
    else:
-      if text in ('3', '4', '5'):
-         start_game(message, int(text))
-         return
-      elif text == 'Yes':
-         start_game(message, user.level)
-         return
-      else:
-         response =  'To start the game write /start or additionaly choose level with /level'
+      bot_answer_not_in_game(message, user)
+
+def bot_answer_not_in_game(message, user):
+   text = message.text
+   if text in ('3', '4', '5'):
+      start_game(message, int(text))
+      return
+   elif text == 'Yes':
+      start_game(message, user.level)
+      return
+   elif not user.mode and text in ('Bot', 'User'):
+      if text == 'Bot':
+         user.mode = 'bot'
+      elif text == 'User':
+         user.mode = 'user'
+      save_user(message.from_user.id, user)
+      start_game(message, user.level)
+      return
+   else:
+      response =  'To start the game write /start or additionaly choose level with /level'
    bot.send_message(message.from_user.id, response)
 
-def get_restart_buttons():
+def bot_answer_to_user_guess(message, user):
+   text = message.text
+   if len(text) == user.level and text.isnumeric() and len(text) == len(set(text)):
+      bulls, cows = get_bulls_cows(text, user.number)
+      user.tries += 1
+      if bulls != user.level:
+         response = f'Bulls: {bulls} | Cows: {cows} | Tries: {user.tries}'
+         save_user(message.from_user.id, user)
+      else:
+         if user.tries <= 3:
+            response = f'You guessed right really fast, only in {user.tries} tries, do you want to play again?'
+            user.reset()
+            save_user(message.from_user.id, user)
+            bot.send_message(message.from_user.id, response, reply_markup=get_buttons('Yes', 'No'))
+            return
+         elif user.tries >= 4 and user.tries < 8:
+            response = f'You guessed right in {user.tries} tries, do you want to play again?'
+            user.reset()
+            save_user(message.from_user.id, user)
+            bot.send_message(message.from_user.id, response, reply_markup=get_buttons('Yes', 'No'))
+            return
+         elif user.tries >= 8:
+            response = f'You guessed right really slow, it took you {user.tries} tries, do you want to play again?'
+            user.reset()
+            save_user(message.from_user.id, user)
+            bot.send_message(message.from_user.id, response, reply_markup=get_buttons('Yes', 'No'))
+            return
+   else:
+      response = f'Send {user.level}-digit number that has unique numbers!'
+   bot.send_message(message.from_user.id, response)
+
+def get_buttons(*args):
    buttons = telebot.types.ReplyKeyboardMarkup(
       one_time_keyboard=True,
       resize_keyboard=True,
    )
-   buttons.add('Yes', 'No')
+   buttons.add(*args)
    return buttons
 
 def get_bulls_cows(text1, text2):
