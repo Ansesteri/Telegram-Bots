a1e1ae842e6e2fc13a80d17f601adad46dc471b1
Ansesteri <angrylemon5678@gmail.com>
2023-06-17 11:56:14 +0300

    Multiuser mod

--- start_bot.py
+++ start_bot.py
@@ -1,18 +1,17 @@
 import random
+import shelve
 import string
 
 import telebot
 
-from config import BOT_TOKEN
+from config import BOT_TOKEN, DB_NAME
 
 bot = telebot.TeleBot(BOT_TOKEN)
-guessed_number = ''
-tries = -1 # tries = -1 if game is not active
+
 
 @bot.message_handler(commands=['start', 'game'])
 def start_game(message):
    digits = [s for s in string.digits]
-   global guessed_number, tries
    guessed_number = ''
    for pos in range(4):
       if pos:
@@ -21,8 +20,9 @@ def start_game(message):
          digit = random.choice(digits[1:])
       guessed_number += digit
       digits.remove(digit)
-   print(guessed_number)
-   tries = 0
+   print(f'{guessed_number} for {message.from_user.username}')
+   with shelve.open(DB_NAME) as storage:
+      storage[str(message.from_user.id)] = (guessed_number, 0)
    bot.reply_to(message, 'Game "Bulls and Cows"\n' 
                 f'I guessed a 4-digit number, try to guess, {message.from_user.first_name}!')
 
@@ -37,37 +37,44 @@ Number is 4-digit without repeated numbers in it (ex. : 2139).
 
 @bot.message_handler(content_types=['text'])
 def bot_answer(message):
-   global tries
    text = message.text
-   if tries == -1:
+   try:
+      with shelve.open(DB_NAME) as storage:
+         guessed_number, tries = storage[str(message.from_user.id)]
+      if len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
+         bulls, cows = get_bulls_cows(text, guessed_number)
+         tries += 1
+         if bulls == 4:
+            if tries <= 3:
+               response = f'You guessed right really fast, only in {tries} tries, do you want to play again?'
+               with shelve.open(DB_NAME) as storage:
+                  del storage[str(message.from_user.id)]
+               bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
+               return
+            elif tries >= 4 and tries < 8:
+               response = f'You guessed right in {tries} tries, do you want to play again?'
+               with shelve.open(DB_NAME) as storage:
+                  del storage[str(message.from_user.id)]
+               bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
+               return
+            elif tries >= 8:
+               response = f'You guessed right really slow, it took you {tries} tries, do you want to play again?'
+               with shelve.open(DB_NAME) as storage:
+                  del storage[str(message.from_user.id)]
+               bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
+               return
+         else:
+            response = f'Bulls: {bulls} | Cows: {cows} | Tries: {tries}'
+            with shelve.open(DB_NAME) as storage:
+               storage[str(message.from_user.id)] = (guessed_number, tries)
+      else:
+         response = 'Send 4-digit number that has unique numbers!'
+   except KeyError:
       if text == 'Yes':
          start_game(message)
          return
       else:
          response =  'To start the game write /start'
-   elif len(text) == 4 and text.isnumeric() and len(text) == len(set(text)):
-      bulls, cows = get_bulls_cows(text, guessed_number)
-      tries += 1
-      if bulls == 4:
-         if tries <= 3:
-            response = f'You guessed right really fast, only in {tries} tries, do you want to play again?'
-            bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
-            tries = -1
-            return
-         elif tries >= 4 and tries < 8:
-            response = f'You guessed right in {tries} tries, do you want to play again?'
-            bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
-            tries = -1
-            return
-         elif tries >= 8:
-            response = f'You guessed right really slow, it took you {tries} tries, do you want to play again?'
-            bot.send_message(message.from_user.id, response, reply_markup=get_restart_buttons())
-            tries = -1
-            return
-      else:
-         response = f'Bulls: {bulls} | Cows: {cows} | Tries: {tries}'
-   else:
-      response = 'Send 4-digit number that has unique numbers!'
    bot.send_message(message.from_user.id, response)
 
 def get_restart_buttons():
